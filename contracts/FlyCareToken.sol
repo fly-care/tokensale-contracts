599cc1ed814ecb264337298cf319b586b6c403ea
Steve Degosserie <steve@block0.io>
2018-02-11 21:24:07 +0100

    Initial commit

---
+++ contracts/FlyCareToken.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.18;
+
+import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "zeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
+import "zeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
+
+
+contract FlyCareToken is MintableToken, PausableToken, BurnableToken {
+
+    string public constant name = "FlyCare Token";
+    string public constant symbol = "FCC";
+    uint8 public constant decimals = 18;
+
+    function FlyCareToken() {
+        pause();
+    }
+}
