07613782f50739f26531b3b640f7215d7447bdd3
Thibault Dory <thibault@block0.io>
2018-05-10 10:42:30 +0200

    Mainnet deployment values for rates, goal and whitelisted address

--- utils/get_rates.py
+++ utils/get_rates.py
@@ -5,5 +5,10 @@ def get_rates(eth_price=300):
         rates.append(round(eth_price / price))
     return rates
 
+def get_goal_in_eth(eth_price=300):
+    goal_in_eur = 1000000.0
+    return round(goal_in_eur / eth_price)
+
 eth_price = int(raw_input('Current ETH price in euros?\n'))
-print get_rates(eth_price)
+print "FCC rates: %s" % get_rates(eth_price)
+print "Goal in ETH: %s" % get_goal_in_eth(eth_price)
